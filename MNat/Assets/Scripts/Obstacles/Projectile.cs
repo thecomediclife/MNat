015e61cd14a9f411ed75102a8e0137672f6f5a89
Chris Ng <thecomediclife324@gmail.com>
2015-07-14 17:12:45 -0700

    Initializing2

---
+++ MNat/Assets/Scripts/Obstacles/Projectile.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour {
+
+	public float waitTime = 4f;
+
+	void Start ()
+	{
+		StartCoroutine (WaitToDestruct (waitTime));
+
+	}
+
+	IEnumerator WaitToDestruct (float waitTime) {
+		yield return new WaitForSeconds(waitTime);
+		Destroy (gameObject);
+	}
+
+	void OnTriggerEnter (Collider other) {
+		if (other.tag != "Obstacle")
+			Destroy (gameObject);
+	}
+}
