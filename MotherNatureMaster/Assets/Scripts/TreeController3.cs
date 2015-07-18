91ce78624f7a9ce4434da51f9f5138a74ad69dd6
kennethlng <ng.kennethlee@gmail.com>
2015-07-18 04:13:57 -0700

    Updated TreeController3.cs
    TreeController3.cs successfully allows player to change tree height
    depending on how far they drag the mouse on the screen. Inspector
    allows developer to decide tree heights and drag limits.

--- MotherNatureMaster/Assets/Scripts/TreeController3.cs
+++ MotherNatureMaster/Assets/Scripts/TreeController3.cs
@@ -7,7 +7,7 @@ public class TreeController3 : MonoBehaviour {
 	private Transform platform;
 	private bool boyOnPlatform;
 
-	public bool dragging;
+	private bool dragging;
 	private float dragStart;
 	private float dragEnd;
 	private float dragDist;
@@ -19,107 +19,52 @@ public class TreeController3 : MonoBehaviour {
 	public int floorNum = 0;
 	public float[] floors; 
 
+	private Vector3 groundPos;
+	private Vector3 startPos;
+	private Vector3 endPos;
 
-
-	void Start () 
+	void Awake ()
 	{
 		platform = transform.FindChild ("Leaves");
 	}
 
+	void Start () 
+	{
+		groundPos = platform.transform.position;
+	}
+
 	void Update () 
 	{
-		//	Check if boy is within Platform's trigger collider & dragging is true
-		if (boyOnPlatform && dragging)
-			boy.transform.parent = transform;
-		else if (boy != null)
-			boy.transform.parent = null;
+//		//	Check if boy is within Platform's trigger collider & dragging is true
+//		if (boyOnPlatform && dragging)
+//			boy.transform.parent = transform;
+//		else if (boy != null)
+//			boy.transform.parent = null;
 
+		CalculateTouchDrag ();
 
-		CalculateDrag ();
-
-
-//		if (!dragging) 
-//		{
-			DetermineFloor ();
-//		}
+		ActivatePlatform (startPos, endPos);
 	}
 
-
-	void ActivatePlatform (Vector3 startPos, Vector3 endPos) 
+	/////////////////////////////////////////////////////////////////////////////////////////////////////////
+	//	This function lerps from start position to end position.
+	void ActivatePlatform (Vector3 start, Vector3 end) 
 	{
-		bool activating = true;
-
-		//	Growing/Rotting function
-		while (activating) 
+		percent += platformSpeed * Time.deltaTime;
+		platform.position = Vector3.Lerp (start, end, percent);
+		
+		if (Vector3.Distance(platform.position, end) < 0.01f)
 		{
-			percent += platformSpeed * Time.deltaTime;
-			platform.position = Vector3.Lerp (startPos, endPos, percent);
-
-			if (Vector3.Distance(platform.position, endPos) < 0.01f)
-			{
-				activating = false;
-			}
+			percent = 0;
+			startPos = end;
 		}
 	}
 
-	void DetermineFloor () 
-	{
-		bool counting = true;
-
-		while (counting) {
-//		if (floors != null) 
-//		{
-			//	Big flick up
-			if (dragDist >= dragLimit) {
-				floorNum += 2;
-				counting = false;
-			}
-			//	Small flick up
-			else if (0 < dragDist && dragDist < dragLimit) {
-				floorNum += 1;
-				counting = false;
-			}
-
-			//	Small flick down
-			else if (-dragLimit < dragDist && dragDist < 0) {
-				floorNum -= 1;
-				counting = false;
-			}
-			//	Big flick down
-			else if (dragDist <= -dragLimit) {
-				floorNum -= 2;
-				counting = false;
-			} 
-
-			else {
-				counting = false;
-			}
-			print (floorNum);
-		}
-
-//			if (floorNum < 0)
-//				floorNum = 0;
-//			if (floorNum > floors.Length)
-//				floorNum = floors.Length;
-
-
-//		}
-	}
-
-
-	void OnTriggerEnter (Collider other) 
-	{
-		boyOnPlatform = true;
-	}
-
-	void OnTriggerExit (Collider other)
-	{
-		boyOnPlatform = false;
-	}
-
-
-
-	void CalculateDrag ()
+	/////////////////////////////////////////////////////////////////////////////////////////////////////////
+	//	This function calculates the Y distance from when the player touched the screen to when the player
+	//	lifted his finger from the screen. Once player lifts finger and the distance is calculated, go to 
+	//	DetermineFloor() function to see how high the tree should grow depending on the drag distance. 
+	void CalculateTouchDrag ()
 	{
 		if (Input.GetButtonDown ("Fire1")) 
 		{
@@ -152,8 +97,54 @@ public class TreeController3 : MonoBehaviour {
 				dragDist = dragEnd - dragStart;
 				dragging = false;
 
-				print ("Drag distance is  " + dragDist);
+				DetermineFloor();
 			}
 		}
+	}
+
+	/////////////////////////////////////////////////////////////////////////////////////////////////////////
+	//	This function is called at the end of CalculateTouchDrag(). Depending on how far the player dragged 
+	//	his finger, DetermineFloor() will calculate how high the tree grows or rots--calculates end position 
+	//	for ActivatePlatform(). 
+	void DetermineFloor () 
+	{
+		if (floors != null) {
+				
+			//	Big flick up
+			if (dragDist >= dragLimit) {
+				floorNum += 2;
+			}
+			//	Small flick up
+			else if (0 < dragDist && dragDist < dragLimit) {
+				floorNum += 1;
+			}
+			//	Small flick down
+			else if (-dragLimit < dragDist && dragDist < 0) {
+				floorNum -= 1;
+			}
+			//	Big flick down
+			else if (dragDist <= -dragLimit) {
+				floorNum -= 2;
+			} else {
+			}
+
+			floorNum = Mathf.Clamp (floorNum, 0, floors.Length - 1);
+			endPos = groundPos + new Vector3 (0, floors [floorNum], 0);
+
+			//	Notes:
+			//	Because arrays start from index 0, floorNum = 0 refers to array [0]. In the inspector
+			//	array [0] is always 0, or basically ground level for the platform. Array[1] is the 
+			//	first desired floor height relative to the ground level. 
+		}
+	}
+
+	void OnTriggerEnter (Collider other) 
+	{
+		boyOnPlatform = true;
+	}
+	
+	void OnTriggerExit (Collider other)
+	{
+		boyOnPlatform = false;
 	}
 }
