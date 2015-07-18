a7afb9991f8a7ad5693a1bd7304e659efd34cf38
kennethlng <ng.kennethlee@gmail.com>
2015-07-18 02:16:21 -0700

    Added tags, layers; updated TreeController3.cs & SceneKenneth3

--- MotherNatureMaster/Assets/Scripts/TreeController3.cs
+++ MotherNatureMaster/Assets/Scripts/TreeController3.cs
@@ -4,68 +4,50 @@ using System.Collections;
 public class TreeController3 : MonoBehaviour {
 
 	private Transform boy;
-	public Transform platform;
+	private Transform platform;
 	private bool boyOnPlatform;
 
-	public bool buttonDown;
-	public bool touchDrag;
-	public bool activating = false;
-	public bool platformHeightReached = false;
+	public bool dragging;
+	private float dragStart;
+	private float dragEnd;
+	private float dragDist;
+	public float dragLimit = 2;
 
 	private float percent = 0;
 	public float platformSpeed = 2f;
-	private Vector3 direction;
-	public Vector3 startPos;
-	public Vector3 endPos;
+
+	public int floorNum = 0;
+	public float[] floors; 
+
 
 
 	void Start () 
 	{
-	
+		platform = transform.FindChild ("Leaves");
 	}
 
 	void Update () 
 	{
-		//	Check if boy is within Platform's trigger collider & buttonDown is true
-		if (boyOnPlatform && buttonDown)
+		//	Check if boy is within Platform's trigger collider & dragging is true
+		if (boyOnPlatform && dragging)
 			boy.transform.parent = transform;
-//		else
-//			boy.transform.parent = null;
-
-		//	Activate platform only when buttondown is true 
-		if (buttonDown) {
-			//	Check if platform is at highest point (true) or at lowest point (false)
-			if (!platformHeightReached) {
-				//	If platform is at lowest point, grow
-				ActivatePlatform (true);
-			} else {
-				//	If platform is at highest point, rot
-				ActivatePlatform (false);
-			}
-		}
+		else if (boy != null)
+			boy.transform.parent = null;
 
 
+		CalculateDrag ();
+
+
+//		if (!dragging) 
+//		{
+			DetermineFloor ();
+//		}
 	}
 
 
-	void ActivatePlatform (bool grow) 
+	void ActivatePlatform (Vector3 startPos, Vector3 endPos) 
 	{
-		activating = true;
-
-		//	Change startPos and endPos depending on if growing or rotting
-		switch (grow) 
-		{
-		//	Growing
-		case true:		
-			//	startPos = ;
-			//	endPos = ;
-			break;
-		//	Rotting
-		case false:				
-			//	startPos = ;
-			//	endPos = ;
-			break;
-		}
+		bool activating = true;
 
 		//	Growing/Rotting function
 		while (activating) 
@@ -76,9 +58,52 @@ public class TreeController3 : MonoBehaviour {
 			if (Vector3.Distance(platform.position, endPos) < 0.01f)
 			{
 				activating = false;
-				platformHeightReached = !platformHeightReached;
 			}
 		}
+	}
+
+	void DetermineFloor () 
+	{
+		bool counting = true;
+
+		while (counting) {
+//		if (floors != null) 
+//		{
+			//	Big flick up
+			if (dragDist >= dragLimit) {
+				floorNum += 2;
+				counting = false;
+			}
+			//	Small flick up
+			else if (0 < dragDist && dragDist < dragLimit) {
+				floorNum += 1;
+				counting = false;
+			}
+
+			//	Small flick down
+			else if (-dragLimit < dragDist && dragDist < 0) {
+				floorNum -= 1;
+				counting = false;
+			}
+			//	Big flick down
+			else if (dragDist <= -dragLimit) {
+				floorNum -= 2;
+				counting = false;
+			} 
+
+			else {
+				counting = false;
+			}
+			print (floorNum);
+		}
+
+//			if (floorNum < 0)
+//				floorNum = 0;
+//			if (floorNum > floors.Length)
+//				floorNum = floors.Length;
+
+
+//		}
 	}
 
 
@@ -94,33 +119,41 @@ public class TreeController3 : MonoBehaviour {
 
 
 
-	void OnMouseDown ()
+	void CalculateDrag ()
 	{
+		if (Input.GetButtonDown ("Fire1")) 
+		{
+			int groundLayerMask = 1 << 9;
+			RaycastHit hitDirt;
+			Vector3 point = Camera.main.ScreenToWorldPoint (Input.mousePosition + new Vector3(0,0,1));
 
+			if (Physics.Raycast (point, Camera.main.transform.forward, out hitDirt, Mathf.Infinity, groundLayerMask)) 
+			{
+				Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+				RaycastHit hit;
+				if (Physics.Raycast(ray, out hit))
+				{	
+					dragStart = hit.point.y;
+				}
 
-		print (Input.mousePosition);
+				dragging = true;
+			}
+		}
 
-//		int groundLayerMask = 1 << 9;
-//
-//		RaycastHit hit;
-//		if (Input.GetButtonDown ("Fire1")) {
-//			Vector3 point = Camera.main.ScreenToWorldPoint (Input.mousePosition);
-//			
-//			//	Shoot raycast, ignore all layers except layer 10 (Trigger)
-//			/*			if (Physics.Raycast (point, Camera.main.transform.forward, out hit, Mathf.Infinity, triggerLayerMask)) {
-//					hit.transform.GetComponent<TreeController2>().buttonDown = true;
-//			}*/
-//			
-//			if (Physics.Raycast (point, Camera.main.transform.forward, out hit, Mathf.Infinity, groundLayerMask)) {
-////				if (hit.transform.parent != null)
-////					hit.transform.parent.transform.GetComponent<TreeController2>().buttonDown = true;
-//			}
-//			
-//		}
-	}
+		if (dragging) 
+		{
+			if (Input.GetButtonUp ("Fire1")) {
+				Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+				RaycastHit hit;
+				if (Physics.Raycast (ray, out hit)) {
+					dragEnd = hit.point.y;
+				}
 
-	void OnMouseUp ()
-	{
+				dragDist = dragEnd - dragStart;
+				dragging = false;
 
+				print ("Drag distance is  " + dragDist);
+			}
+		}
 	}
 }
