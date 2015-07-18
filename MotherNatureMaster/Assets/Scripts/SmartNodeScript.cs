dc354b425222a4ed025e781f3cbf5a036096be27
Chris Ng <thecomediclife324@gmail.com>
2015-07-18 01:42:40 -0700

    fixed

---
+++ MotherNatureMaster/Assets/Scripts/SmartNodeScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SmartNodeScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.tag == "Player") {
+			other.GetComponent<CharController6>().Pause(transform, true, transform.position + new Vector3(0,0,-1));
+		}
+	}
+}
