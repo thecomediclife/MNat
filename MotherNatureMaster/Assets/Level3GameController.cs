c33d2d520beccd6359f72c70328e3660a4d4821a
Chris Ng <thecomediclife324@gmail.com>
2015-08-13 22:54:57 -0700

    Level3 Begun

---
+++ MotherNatureMaster/Assets/Level3GameController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Level3GameController : MonoBehaviour {
+	public bool checkpoint1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ MotherNatureMaster/Assets/Level3Gate.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Level3Gate : MonoBehaviour {
+	public bool open, key1, key2 = false;
+
+	private Transform door1;
+	private Transform door2;
+
+	// Use this for initialization
+	void Start () {
+		door1 = transform.Find ("Door1");
+		door2 = transform.Find ("Door2");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (key1 && key2)
+			open = true;
+
+		if (open)
+			OpenDoors ();
+
+	}
+
+	void OpenDoors() {
+		if (door2.transform.rotation.eulerAngles.y < 45f) {
+			door1.transform.Rotate (new Vector3 (0, -5f, 0) * Time.deltaTime);
+			door2.transform.Rotate (new Vector3 (0, 5f, 0) * Time.deltaTime);
+		}
+	}
+}
