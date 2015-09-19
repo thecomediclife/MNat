cee90ce281ea14d7902c20d02f651b017663aa78
kennethlng <ng.kennethlee@gmail.com>
2015-09-19 14:59:50 -0700

    SheeShee

---
+++ MotherNatureMaster/Assets/Scripts/FishController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FishController : MonoBehaviour {
+
+	public Vector3 sizeOfSphere;
+	private Vector3 nextDest;
+	private Vector3 dir;
+	private float speed;
+
+	void Start () 
+	{
+		nextDest = calcDest ();
+		speed = 1;
+	}
+
+	void Update () 
+	{
+		transform.localPosition = Vector3.MoveTowards (transform.localPosition, nextDest, speed * Time.deltaTime);
+
+		transform.LookAt (nextDest + transform.parent.transform.position);
+
+
+		if (Vector3.Distance (transform.localPosition, nextDest) < 0.05) {
+			transform.localPosition = nextDest;
+			nextDest = calcDest();
+
+			speed = Random.Range(0.2f, 1.5f);
+		}
+	}
+
+	public Vector3 calcDest() 
+	{
+		Vector3 newDest = Random.insideUnitSphere;
+		float newDestX = newDest.x * sizeOfSphere.x;
+		float newDestY = newDest.y * sizeOfSphere.y;
+		float newDestZ = newDest.z * sizeOfSphere.z;
+		newDest = new Vector3 (newDestX, newDestY, newDestZ);
+
+		return newDest;
+
+	}
+}
