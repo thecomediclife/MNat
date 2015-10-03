c3e67357ced58fd2c82d277f3bc09436aa229a40
thecomediclife <thecomediclife324@gmail.com>
2015-10-02 17:17:59 -0700

    Big Commit

--- MotherNatureMaster/Assets/Scripts/BombScript.cs
+++ MotherNatureMaster/Assets/Scripts/BombScript.cs
@@ -4,13 +4,15 @@ using System.Collections;
 public class BombScript : MonoBehaviour {
 	public bool activate = false;
 
-	public float detonateTime = 10f;
+	public float detonateTime = 7f;
 
 	private float timer;
 
 	public Material defaultMat, redMat;
 
 	private Transform bomb;
+
+	public bool detonate;
 
 	void Awake() {
 		bomb = transform.GetChild (0);
@@ -19,22 +21,23 @@ public class BombScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (activate) {
-			if (Time.time > timer - 6f && Time.time < timer - 2f) {
+			if (Time.time > timer - detonateTime * 0.6f && Time.time < timer - detonateTime * 0.2f) {
 				bomb.GetComponent<Animator> ().SetInteger ("BlipState", 2);
 			}
 
-			if (Time.time > timer - 2f) {
+			if (Time.time > timer - detonateTime * 0.2f) {
 				bomb.GetComponent<Animator> ().SetInteger ("BlipState", 3);
 			}
 
 			if (Time.time > timer) {
-				Debug.Log ("detonate");
+//				Debug.Log ("detonate");
+				detonate = true;
 			}
 		}
 
-		if (Input.GetKeyDown(KeyCode.Z)) {
-			SetActive();
-		}
+//		if (Input.GetKeyDown(KeyCode.Z)) {
+//			SetActive();
+//		}
 	}
 
 	public void SetActive() {
@@ -43,4 +46,9 @@ public class BombScript : MonoBehaviour {
 		bomb.GetComponent<Animator> ().SetInteger ("BlipState", 1);
 	}
 
+	public void Reset() {
+		activate = false;
+		detonate = false;
+		bomb.GetComponent<Animator> ().SetInteger ("BlipState", 0);
+	}
 }
